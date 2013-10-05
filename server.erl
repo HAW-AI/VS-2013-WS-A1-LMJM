79dc6a3130215f01fd468d311edb153e65208145
Jan Menzel <jan.menzel@haw-hamburg.de>
2013-10-05 15:27:41 +0200

    Add .erl~ to gitignore, Simple implementation of server/client interface

--- server.erl
+++ server.erl
@@ -1,8 +1,12 @@
 -module(server).
 -compile(export_all).
 
--record(state, {current_msg_id=0}).
--record(message_id = 0).
+-record(state, {
+                current_msg_id=0,
+                clients = [],
+                hold_back_queue = [],
+                delivery_queue = []
+               }).
 
 start() ->
     State = #state{},
@@ -14,13 +18,28 @@ start() ->
 loop(State) ->
     receive
         {getmessages, PID} ->
-            PID ! {State#state.current_msg_id, self()},
-            NewState = #state{ current_msg_id= State#state.current_msg_id + 1},
+            %{reply,Number,Nachricht,Terminated}
+            {Number, Nachricht} = get_last_element(State#state.hold_back_queue),
+            PID ! {reply, Number, Nachricht, false},
+            loop(State);
+        {getmsgid, PID} ->
+            NewState = inc_message_id(State),
+            io:format("Send id " ++ NewState#state.current_msg_id),
+            PID ! {nid, NewState#state.current_msg_id},
+            loop(NewState);
+        {dropmessage, {Nachricht, Number}} ->
+            NewState = put_message(Number, Nachricht, State),
+            io:format("Got Message " ++ Nachricht),
             loop(NewState)
+            
     end.
 
-send(Pid) ->
-    Pid ! {getmessages, self()},
-    receive {NR, NPid} ->
-        {NR, NPid}
-    end.
+inc_message_id(State) ->
+    #state{ current_msg_id= State#state.current_msg_id + 1}.
+
+put_message(Number, Message, State) ->
+    #state{hold_back_queue = State#state.hold_back_queue ++ {Number, Message}}.
+
+get_last_element([]) -> [];
+get_last_element([Head | []]) -> Head;
+get_last_element([_|Tail]) -> get_last_element(Tail).
