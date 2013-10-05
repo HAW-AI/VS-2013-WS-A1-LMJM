9fff06e468cb4f615001d7846877519e5770a50d
Luciano Maiwald <luciano.maiwald@gmail.com>
2013-10-05 18:18:43 +0200

    fixed indentation

--- server.erl
+++ server.erl
@@ -2,40 +2,40 @@
 -compile(export_all).
 
 -record(state, {
-                current_msg_id=0,
-                clients = [],
-                hold_back_queue = [],
-                delivery_queue = []
-               }).
+        current_msg_id=0,
+        clients = [],
+        hold_back_queue = [],
+        delivery_queue = []
+         }).
 
 start() ->
-    State = #state{},
-    ServerPID = spawn(fun() -> loop(State) end),
-    register(wk, ServerPID),
-    %io:format("Server started PID =  ~s!", [ServerPID]),
-    ServerPID.
+  State = #state{},
+  ServerPID = spawn(fun() -> loop(State) end),
+  register(wk, ServerPID),
+  %io:format("Server started PID =  ~s!", [ServerPID]),
+  ServerPID.
 
 loop(State) ->
-    receive
-        {getmessages, PID} ->
-            %{reply,Number,Nachricht,Terminated}
-            {Number, Nachricht} = lists:last(State#state.hold_back_queue),
-            PID ! {reply, Number, Nachricht, false},
-            loop(State);
-        {getmsgid, PID} ->
-            NewState = inc_message_id(State),
-            io:format("Send id ~b", [NewState#state.current_msg_id]),
-            PID ! {nid, NewState#state.current_msg_id},
-            loop(NewState);
-        {dropmessage, {Nachricht, Number}} ->
-            NewState = put_message(Number, Nachricht, State),
-            io:format("Got Message " ++ Nachricht),
-            loop(NewState)
+  receive
+    {getmessages, PID} ->
+      %{reply,Number,Nachricht,Terminated}
+      {Number, Nachricht} = lists:last(State#state.hold_back_queue),
+      PID ! {reply, Number, Nachricht, false},
+      loop(State);
+    {getmsgid, PID} ->
+      NewState = inc_message_id(State),
+      io:format("Send id ~b", [NewState#state.current_msg_id]),
+      PID ! {nid, NewState#state.current_msg_id},
+      loop(NewState);
+    {dropmessage, {Nachricht, Number}} ->
+      NewState = put_message(Number, Nachricht, State),
+      io:format("Got Message " ++ Nachricht),
+      loop(NewState)
 
-    end.
+  end.
 
 inc_message_id(State) ->
-    State#state{ current_msg_id= State#state.current_msg_id + 1}.
+  State#state{ current_msg_id= State#state.current_msg_id + 1}.
 
 put_message(Number, Message, State) ->
-    State#state{hold_back_queue = State#state.hold_back_queue ++ [{Number, Message}]}.
+  State#state{hold_back_queue = State#state.hold_back_queue ++ [{Number, Message}]}.
