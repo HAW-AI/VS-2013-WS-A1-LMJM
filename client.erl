99d413f62b49aae168ca91edd40ed228956d358a
Luciano Maiwald <luciano.maiwald@gmail.com>
2013-10-05 18:19:48 +0200

    when you send the right message, you get the right answer…

--- client.erl
+++ client.erl
@@ -9,24 +9,24 @@ start(Server) ->
   spawn(fun() -> editor(Server, 5) end).
 
 editor(Server, Lives) ->
-  Server ! { getmsgid, self() },
+  Server ! {getmsgid, self()},
   receive
     { nid, MessageId } ->
       log("~p Received MessageId ~b", [self(), MessageId]),
       timer:sleep(?MESSAGE_INTERVAL),
-      Server ! { "My fancy Message", MessageId }
+      Server ! {dropmessage, {"My fancy Message", MessageId}}
   end,
 
-  %Server ! { getmessages, self() },
-  %receive
-    %{ reply, MessageId, Message, Terminated } ->
-      %log("received message ~b ~p", [MessageId, Message])
-  %end,
+  Server ! {getmessages, self()},
+  receive
+    {reply, MessageId, Message, _Terminated} ->
+      log("received message ~b ~p", [MessageId, Message])
+  end,
 
   if
-    Lives > 0 ->
-      editor(Server, Lives - 1)
-  end.
+    (Lives > 0) -> editor(Server, Lives - 1);
+    true -> true
+  end .
 
 log(Format, Data) ->
   logging("client.log", io_lib:format(Format ++ "~n", Data)).
--- server.erl
+++ server.erl
@@ -1,41 +1,48 @@
 -module(server).
--compile(export_all).
+-export([start/0]).
+
+-import(werkzeug, [logging/2]).
 
 -record(state, {
-        current_msg_id=0,
-        clients = [],
-        hold_back_queue = [],
-        delivery_queue = []
-         }).
+  current_msg_id  = 0,
+  clients         = [],
+  hold_back_queue = [],
+  delivery_queue  = []
+}).
 
 start() ->
   State = #state{},
   ServerPID = spawn(fun() -> loop(State) end),
   register(wk, ServerPID),
-  %io:format("Server started PID =  ~s!", [ServerPID]),
+  log("Server started PID = ~p!", [ServerPID]),
   ServerPID.
 
 loop(State) ->
   receive
     {getmessages, PID} ->
-      %{reply,Number,Nachricht,Terminated}
       {Number, Nachricht} = lists:last(State#state.hold_back_queue),
       PID ! {reply, Number, Nachricht, false},
       loop(State);
     {getmsgid, PID} ->
       NewState = inc_message_id(State),
-      io:format("Send id ~b", [NewState#state.current_msg_id]),
+      log("Send id ~b", [NewState#state.current_msg_id]),
       PID ! {nid, NewState#state.current_msg_id},
       loop(NewState);
-    {dropmessage, {Nachricht, Number}} ->
-      NewState = put_message(Number, Nachricht, State),
-      io:format("Got Message " ++ Nachricht),
+    {dropmessage, {Message, Number}} ->
+      NewState = put_message(Number, Message, State),
+      log("Got Message ~s", [Message]),
       loop(NewState)
 
   end.
 
 inc_message_id(State) ->
-  State#state{ current_msg_id= State#state.current_msg_id + 1}.
+  State#state{current_msg_id = State#state.current_msg_id + 1}.
 
 put_message(Number, Message, State) ->
   State#state{hold_back_queue = State#state.hold_back_queue ++ [{Number, Message}]}.
+
+log(Format, Data) ->
+  logging("server.log", io_lib:format(Format ++ "~n", Data)).
+
+log(Message) ->
+  logging("server.log", Message ++ "~n").
