65b55651d3342ba8b2d53cface986481fe573aee
Luciano Maiwald <luciano.maiwald@gmail.com>
2013-10-06 11:34:52 +0200

    added client config

--- client.erl
+++ client.erl
@@ -3,36 +3,54 @@
 
 -import(werkzeug, [logging/2]).
 
--define(MESSAGE_INTERVAL, 300).
+-record(config, {
+  server,
+  server_name,
+  number_of_clients,
+  lifetime,
+  message_interval
+}).
 
 start(Server) ->
+  Config = load_config(Server),
+  span_clients(Config).
+
+load_config(Server) ->
+  {ok, ConfigFile} = file:consult('client.cfg'),
+  #config{
+    server            = Server,
+    server_name       = proplists:get_value(servername, ConfigFile),
+    number_of_clients = proplists:get_value(clients, ConfigFile),
+    lifetime          = proplists:get_value(lifetime, ConfigFile),
+    message_interval  = proplists:get_value(sendeintervall, ConfigFile) * 100
+  }.
+
+span_clients(Config) ->
   lists:foreach(fun(I) ->
-    spawn(fun() ->
-      editor(Server, 5 + I)
-    end),
+    spawn(fun() -> editor(Config, Config#config.lifetime) end),
     timer:sleep(I * 1000)
+  end, lists:seq(1, Config#config.number_of_clients)).
 
-  end, lists:seq(0, 4)).
-
-editor(Server, Lives) ->
-  Server ! {getmsgid, self()},
+editor(Config, Lives) ->
+  Config#config.server ! {getmsgid, self()},
   receive
     {nid, MessageId} ->
       log("~p Received MessageId ~b", [self(), MessageId]),
-      timer:sleep(?MESSAGE_INTERVAL),
-      Server ! {dropmessage, {"My fancy Message", MessageId}}
-  end,
-
-  Server ! {getmessages, self()},
-  receive
-    {reply, MessageId, Message, _Terminated} ->
-      log("received message ~b ~p", [MessageId, Message])
+      timer:sleep(Config#config.message_interval),
+      Config#config.server ! {dropmessage, {"My fancy Message", MessageId}}
   end,
 
   if
-    (Lives > 0) -> editor(Server, Lives - 1);
-    true -> true
-  end .
+    (Lives > 0) -> editor(Config, Lives - 1);
+    true -> reader(Config)
+  end.
+
+reader(Config) ->
+  Config#config.server ! {getmessages, self()},
+  receive
+    {reply, MessageId, Message, _Terminated} ->
+      log("~p Received message ~b ~p", [self(), MessageId, Message])
+  end.
 
 log(Format, Data) ->
   logging("client.log", io_lib:format(Format ++ "~n", Data)).
